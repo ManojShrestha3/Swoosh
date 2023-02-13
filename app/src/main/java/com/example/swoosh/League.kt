cbd33efbd0eb13a59c0f367bc2b02aa6431aaefe
manojshrestha <Manojshrestha3>
2023-02-13 19:11:19 +0900

    lesson 6

--- app/src/main/java/com/example/swoosh/League.kt
+++ app/src/main/java/com/example/swoosh/League.kt
@@ -1,11 +1,18 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class League : AppCompatActivity() {
+class League : BaseAcitivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league4)
+    }
+    fun leagueClicked(view: View){
+        val skillAcitivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillAcitivity)
+
     }
 }
--- app/src/main/java/com/example/swoosh/MainActivity.kt
+++ app/src/main/java/com/example/swoosh/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseAcitivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
